34e4b938c6d8d769485fd16609d45fbff81b3645
svanke <martynas@infinitegrid.xyz>
2018-02-26 20:05:15 +1100

    first commit

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_slug}}/config/settings/local.py
@@ -0,0 +1,47 @@
+from .base import *
+
+# Secret key doesn't matter in local development
+# SECRET_KEY = 'some-keys'
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='CHANGEME!!!')
+
+# Debug mode
+DEBUG = True
+
+# Allowed from all host
+ALLOWED_HOSTS = ['*']
+
+# App specifically for development
+INSTALLED_APPS += [
+    'debug_toolbar',
+    'django_extensions',
+]
+
+# Middleware for development
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
+]
+
+# Database configuration
+DATABASES = {
+    'default': env.db("DATABASE_URL", default="postgres://postgres:postgres@localhost:5432/{{cookiecutter.database_name}}")
+}
+DATABASES['default']['ATOMIC_REQUESTS'] = True
+
+# Django REST Framework Cors Header Origin
+CORS_ORIGIN_ALLOW_ALL = True
+
+# Static files
+STATIC_ROOT = str(ROOT_DIR('static_files'))
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    str(ROOT_DIR.path('static')),
+)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+# Media files
+MEDIA_ROOT = str(ROOT_DIR('media'))
+MEDIA_URL = '/media/'
